0c2e488bbaec8f4b99c4d8cf8a1d6929d8da93a2
siad007 <siad.ardroumli@gmail.com>
2017-05-07 16:54:21 +0200

    Added bootstrap.

--- src/Module.php
+++ src/Module.php
@@ -3,8 +3,11 @@
 namespace Siad007\ZF2\ReactJsModule;
 
 use Siad007\ZF2\ReactJsModule\Service\ReactRendererStrategyFactory;
+use Siad007\ZF2\ReactJsModule\Strategy\ReactRendererStrategy;
+use Zend\EventManager\EventInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\Mvc\MvcEvent;
 
 class Module implements ConfigProviderInterface, ServiceProviderInterface
 {
@@ -31,5 +34,18 @@ class Module implements ConfigProviderInterface, ServiceProviderInterface
                 Strategy\ReactRendererStrategy::class => ReactRendererStrategyFactory::class,
             ]
         ];
+    }
+
+    public function onBootstrap(EventInterface $e)
+    {
+        /** @var MvcEvent $e */
+        $sharedEvents = $e->getApplication()->getEventManager()->getSharedManager();
+        $sm = $e->getApplication()->getServiceManager();
+
+        $sharedEvents->attach(__NAMESPACE__, MvcEvent::EVENT_DISPATCH, function($e) use ($sm) {
+            $strategy = $sm->get(ReactRendererStrategy::class);
+            $view     = $sm->get('ViewManager')->getView();
+            $strategy->attach($view->getEventManager());
+        }, 100);
     }
 }
