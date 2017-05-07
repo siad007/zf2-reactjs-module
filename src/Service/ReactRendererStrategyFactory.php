6841bcca6898681cf1f2aef73aea8a82cc9606ec
siad007 <siad.ardroumli@gmail.com>
2017-05-07 14:34:59 +0200

    Fixed factory

--- src/Service/ReactRendererStrategyFactory.php
+++ src/Service/ReactRendererStrategyFactory.php
@@ -2,6 +2,7 @@
 
 namespace Siad007\ZF2\ReactJsModule\Service;
 
+use Interop\Container\ContainerInterface;
 use Siad007\ZF2\ReactJsModule\Strategy\ReactRendererStrategy;
 use Zend\ServiceManager\FactoryInterface;
 use Siad007\ZF2\ReactJsModule\Renderer\ReactRenderer;
@@ -9,9 +10,22 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 
 class ReactRendererStrategyFactory implements FactoryInterface
 {
-    public function createService(ServiceLocatorInterface $serviceLocator)
+    public function __invoke(ContainerInterface $container, $name, array $options = null)
     {
-        $viewRenderer = $serviceLocator->get(ReactRenderer::class);
+        $viewRenderer = $container->get(ReactRenderer::class);
         return new ReactRendererStrategy($viewRenderer);
+    }
+
+    /**
+     * Create and return PhpRenderer instance
+     *
+     * For use with zend-servicemanager v2; proxies to __invoke().
+     *
+     * @param ServiceLocatorInterface $container
+     * @return ReactRenderer
+     */
+    public function createService(ServiceLocatorInterface $container)
+    {
+        return $this($container, ReactRenderer::class);
     }
 }
