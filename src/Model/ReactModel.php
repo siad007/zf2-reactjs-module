8b78515e92baae9394848bd7a88f5c019f5f3a80
siad007 <siad.ardroumli@gmail.com>
2017-05-07 16:38:59 +0200

    Added ReactModel

---
+++ src/Model/ReactModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Siad007\ZF2\ReactJsModule\Model;
+
+use Zend\View\Renderer\PhpRenderer;
+
+class ReactModel extends PhpRenderer
+{
+    /**
+     * @param string $component Name of component
+     * @param string $where     Id of container in which component is rendered
+     * @param array  $data      Data to be passed to the component
+     *
+     * @return string
+     */
+    public function getRenderJs($component, $where, array $data = [])
+    {
+        return
+            sprintf(
+                'React.render(React.createElement(%s, %s), %s);',
+                $component,
+                json_encode($data),
+                $where
+            );
+    }
+}
